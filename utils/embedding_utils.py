6ba66e6c0d693d67a35f10a58440dccab805007a
Szilvia Csernusne Berczes <csernus.szilvi@gmail.com>
2024-06-03 13:53:08 +0100

    add utils

---
+++ utils/embedding_utils.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from scipy import spatial
+import numpy as np
+
+def distances_from_embeddings(
+    query_embedding: List[float],
+    embeddings: List[List[float]],
+    distance_metric="cosine",
+) -> List[List]:
+    """Return the distances between a query embedding and a list of embeddings."""
+    distance_metrics = {
+        "cosine": spatial.distance.cosine,
+        "L1": spatial.distance.cityblock,
+        "L2": spatial.distance.euclidean,
+        "Linf": spatial.distance.chebyshev,
+    }
+    distances = [
+        distance_metrics[distance_metric](query_embedding, embedding)
+        for embedding in embeddings
+    ]
+    return distances
+
+
+def indices_of_nearest_neighbors_from_distances(distances) -> np.ndarray:
+    """Return a list of indices of nearest neighbors from a list of distances."""
+    return np.argsort(distances)
